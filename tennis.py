693430d5deb3d60cced1bc0282c6e256902e84d5
Quynh Nguyen <quynh.xq@gmail.com>
2016-04-17 23:37:27 +0200

    Use space before opearands

--- tennis.py
+++ tennis.py
@@ -66,34 +66,34 @@ class TennisGame2:
     def score(self):
         result = ""
         if (self.p1points == self.p2points and self.p1points < 3):
-            if (self.p1points==0):
+            if (self.p1points == 0):
                 result = "Love"
-            if (self.p1points==1):
+            if (self.p1points == 1):
                 result = "Fifteen"
-            if (self.p1points==2):
+            if (self.p1points == 2):
                 result = "Thirty"
             result += "-All"
-        if (self.p1points==self.p2points and self.p1points>2):
+        if (self.p1points == self.p2points and self.p1points>2):
             result = "Deuce"
 
         P1res = ""
         P2res = ""
-        if (self.p1points > 0 and self.p2points==0):
-            if (self.p1points==1):
+        if (self.p1points > 0 and self.p2points == 0):
+            if (self.p1points == 1):
                 P1res = "Fifteen"
-            if (self.p1points==2):
+            if (self.p1points == 2):
                 P1res = "Thirty"
-            if (self.p1points==3):
+            if (self.p1points == 3):
                 P1res = "Forty"
 
             P2res = "Love"
             result = P1res + "-" + P2res
-        if (self.p2points > 0 and self.p1points==0):
-            if (self.p2points==1):
+        if (self.p2points > 0 and self.p1points == 0):
+            if (self.p2points == 1):
                 P2res = "Fifteen"
-            if (self.p2points==2):
+            if (self.p2points == 2):
                 P2res = "Thirty"
-            if (self.p2points==3):
+            if (self.p2points == 3):
                 P2res = "Forty"
 
             P1res = "Love"
@@ -101,24 +101,24 @@ class TennisGame2:
 
 
         if (self.p1points>self.p2points and self.p1points < 4):
-            if (self.p1points==2):
-                P1res="Thirty"
-            if (self.p1points==3):
-                P1res="Forty"
-            if (self.p2points==1):
-                P2res="Fifteen"
-            if (self.p2points==2):
-                P2res="Thirty"
+            if (self.p1points == 2):
+                P1res = "Thirty"
+            if (self.p1points == 3):
+                P1res = "Forty"
+            if (self.p2points == 1):
+                P2res = "Fifteen"
+            if (self.p2points == 2):
+                P2res = "Thirty"
             result = P1res + "-" + P2res
         if (self.p2points>self.p1points and self.p2points < 4):
-            if (self.p2points==2):
-                P2res="Thirty"
-            if (self.p2points==3):
-                P2res="Forty"
-            if (self.p1points==1):
-                P1res="Fifteen"
-            if (self.p1points==2):
-                P1res="Thirty"
+            if (self.p2points == 2):
+                P2res = "Thirty"
+            if (self.p2points == 3):
+                P2res = "Forty"
+            if (self.p1points == 1):
+                P1res = "Fifteen"
+            if (self.p1points == 2):
+                P1res = "Thirty"
             result = P1res + "-" + P2res
 
         if (self.p1points > self.p2points and self.p2points >= 3):
@@ -127,9 +127,9 @@ class TennisGame2:
         if (self.p2points > self.p1points and self.p1points >= 3):
             result = "Advantage " + self.player2Name
 
-        if (self.p1points>=4 and self.p2points>=0 and (self.p1points-self.p2points)>=2):
+        if (self.p1points >= 4 and self.p2points >= 0 and (self.p1points-self.p2points) >= 2):
             result = "Win for " + self.player1Name
-        if (self.p2points>=4 and self.p1points>=0 and (self.p2points-self.p1points)>=2):
+        if (self.p2points >= 4 and self.p1points >= 0 and (self.p2points-self.p1points) >= 2):
             result = "Win for " + self.player2Name
         return result
 
@@ -142,11 +142,11 @@ class TennisGame2:
             self.P2Score()
 
     def P1Score(self):
-        self.p1points +=1
+        self.p1points += 1
 
 
     def P2Score(self):
-        self.p2points +=1
+        self.p2points += 1
 
 class TennisGame3:
     def __init__(self, player1Name, player2Name):
